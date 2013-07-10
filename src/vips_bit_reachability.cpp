926e4c652b2530c607295013e6b21a50c0151fa9
Carl Leonardsson <carl.leonardsson@gmail.com>
2013-07-10 16:35:52 +0200

    Implemented reachability analysis for VIPS.
    VipsBitReachability.

--- src/vips_bit_reachability.cpp
+++ src/vips_bit_reachability.cpp
@@ -21,12 +21,346 @@
 #include "test.h"
 #include "vips_bit_reachability.h"
 
+#include <functional>
 #include <stdexcept>
 
 Reachability::Result *VipsBitReachability::reachability(Arg *arg) const{
-  throw new std::logic_error("VipsBitReachability: Not implemented.");
+  const Machine &machine = arg->machine;
+  Result *result = new Result(machine);
+  result->timer.start();
+  result->result = UNREACHABLE;
+
+  bool found_forbidden = false;
+
+  VipsBitConstraint::Common common(machine);
+
+  /* stack contains constraints that have been found but not explored */
+  std::vector<const VipsBitConstraint*> stack;
+
+  /* The set of keys of visited is the set of visited constraints.
+   * Each visited constraint maps to a description of its parent.
+   */
+  std::map<const VipsBitConstraint*,parent_t,vbcmp_t> visited(get_comparator(common));
+
+  /* Find the initial constraints */
+  {
+    std::set<VipsBitConstraint*> init = common.get_initial_constraints();
+    result->generated_constraints = result->stored_constraints = init.size();
+    for(auto it = init.begin(); it != init.end(); ++it){
+      if((*it)->is_forbidden(common)){
+        result->trace = new Trace(0);
+        result->result = REACHABLE;
+        found_forbidden = true;
+      }
+      stack.push_back(*it);
+      visited[*it] = parent_t();
+    }
+  }
+
+  while(!found_forbidden && stack.size()){
+    const VipsBitConstraint *vbc = stack.back();
+    stack.pop_back();
+
+    VecSet<const Machine::PTransition*> transes = vbc->partred(common);
+
+    for(int i = 0; !found_forbidden && i < transes.size(); ++i){
+      VipsBitConstraint *child = vbc->post(common,*transes[i]);
+      if(child){
+        ++result->generated_constraints;
+        if(visited.count(child)){
+          delete child;
+        }else{
+          visited[child] = parent_t(transes[i],vbc);
+          stack.push_back(child);
+          if(child->is_forbidden(common)){
+            result->result = REACHABLE;
+            found_forbidden = true;
+            /* Construct trace */
+            {
+              result->trace = new Trace(0);
+              const parent_t *pt = &visited[child];
+              while(pt->parent){
+                result->trace->push_front(0,*pt->trans);
+                pt = &visited[pt->parent];
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  result->stored_constraints = visited.size();
+
+  /* Cleanup */
+  for(auto it = visited.begin(); it != visited.end(); ++it){
+    delete it->first;
+  }
+
+  result->timer.stop();
+  return result;
+};
+
+VipsBitReachability::vbcmp_t VipsBitReachability::get_comparator(const VipsBitConstraint::Common &common){
+  return [&common](const VipsBitConstraint *a,const VipsBitConstraint *b){
+    return common.compare(*a,*b) < 0;
+  };
 };
 
 void VipsBitReachability::test(){
-  Test::inner_test("Testing?",true);
+  std::function<Machine*(std::string)> get_machine = 
+    [](std::string rmm){
+    std::stringstream ss(rmm);
+    Lexer lex(ss);
+    return new Machine(Parser::p_test(lex));
+  };
+
+  /* Test 1: Dekker */
+  {
+    Machine *m = get_machine
+      ("forbidden CS CS\n"
+       "data\n"
+       "  x = 0 : [0:1]\n"
+       "  y = 0 : [0:1]\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  write: x := 1;\n"
+       "  read: y = 0;\n"
+       "CS:\n"
+       "  write: x := 0;\n"
+       "  goto L1\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  write: y := 1;\n"
+       "  read: x = 0;\n"
+       "CS:\n"
+       "  write: y := 0;\n"
+       "  goto L1\n"
+       );
+
+    VipsBitReachability reach;
+
+    Arg arg(*m);
+    Result *res = reach.reachability(&arg);
+    Test::inner_test("#1 Simple Dekker",res->result == REACHABLE);
+
+    delete res;
+    delete m;
+  }
+
+  /* Test 2: Dekker 1 syncwr */
+  {
+    Machine *m = get_machine
+      ("forbidden CS CS\n"
+       "data\n"
+       "  x = 0 : [0:1]\n"
+       "  y = 0 : [0:1]\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  syncwr: x := 1;\n"
+       "  read: y = 0;\n"
+       "CS:\n"
+       "  write: x := 0;\n"
+       "  goto L1\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  write: y := 1;\n"
+       "  read: x = 0;\n"
+       "CS:\n"
+       "  write: y := 0;\n"
+       "  goto L1\n"
+       );
+
+    VipsBitReachability reach;
+
+    Arg arg(*m);
+    Result *res = reach.reachability(&arg);
+    Test::inner_test("#2 Simple Dekker",res->result == REACHABLE);
+
+    delete res;
+    delete m;
+  }
+
+  /* Test 3: Dekker 1 syncwr (other process) */
+  {
+    Machine *m = get_machine
+      ("forbidden CS CS\n"
+       "data\n"
+       "  x = 0 : [0:1]\n"
+       "  y = 0 : [0:1]\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  write: x := 1;\n"
+       "  read: y = 0;\n"
+       "CS:\n"
+       "  write: x := 0;\n"
+       "  goto L1\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  syncwr: y := 1;\n"
+       "  read: x = 0;\n"
+       "CS:\n"
+       "  write: y := 0;\n"
+       "  goto L1\n"
+       );
+
+    VipsBitReachability reach;
+
+    Arg arg(*m);
+    Result *res = reach.reachability(&arg);
+    Test::inner_test("#3 Simple Dekker",res->result == REACHABLE);
+
+    delete res;
+    delete m;
+  }
+
+  /* Test 4: Dekker syncwr in both processes */
+  {
+    Machine *m = get_machine
+      ("forbidden CS CS\n"
+       "data\n"
+       "  x = 0 : [0:1]\n"
+       "  y = 0 : [0:1]\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  syncwr: x := 1;\n"
+       "  read: y = 0;\n"
+       "CS:\n"
+       "  write: x := 0;\n"
+       "  goto L1\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  syncwr: y := 1;\n"
+       "  read: x = 0;\n"
+       "CS:\n"
+       "  write: y := 0;\n"
+       "  goto L1\n"
+       );
+
+    VipsBitReachability reach;
+
+    Arg arg(*m);
+    Result *res = reach.reachability(&arg);
+    Test::inner_test("#4 Simple Dekker",res->result == REACHABLE);
+
+    delete res;
+    delete m;
+  }
+
+  /* Test 5: Dekker fence in one process */
+  {
+    Machine *m = get_machine
+      ("forbidden CS CS\n"
+       "data\n"
+       "  x = 0 : [0:1]\n"
+       "  y = 0 : [0:1]\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  write: x := 1;\n"
+       "  fence;\n"
+       "  read: y = 0;\n"
+       "CS:\n"
+       "  write: x := 0;\n"
+       "  goto L1\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  write: y := 1;\n"
+       "  read: x = 0;\n"
+       "CS:\n"
+       "  write: y := 0;\n"
+       "  goto L1\n"
+       );
+
+    VipsBitReachability reach;
+
+    Arg arg(*m);
+    Result *res = reach.reachability(&arg);
+    Test::inner_test("#5 Simple Dekker",res->result == REACHABLE);
+
+    delete res;
+    delete m;
+  }
+  /* Test 6: Dekker fence in one process */
+  {
+    Machine *m = get_machine
+      ("forbidden CS CS\n"
+       "data\n"
+       "  x = 0 : [0:1]\n"
+       "  y = 0 : [0:1]\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  write: x := 1;\n"
+       "  read: y = 0;\n"
+       "CS:\n"
+       "  write: x := 0;\n"
+       "  goto L1\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  write: y := 1;\n"
+       "  fence;\n"
+       "  read: x = 0;\n"
+       "CS:\n"
+       "  write: y := 0;\n"
+       "  goto L1\n"
+       );
+
+    VipsBitReachability reach;
+
+    Arg arg(*m);
+    Result *res = reach.reachability(&arg);
+    Test::inner_test("#6 Simple Dekker",res->result == REACHABLE);
+
+    delete res;
+    delete m;
+  }
+
+  /* Test 7: Dekker fence in both processes */
+  {
+    Machine *m = get_machine
+      ("forbidden CS CS\n"
+       "data\n"
+       "  x = 0 : [0:1]\n"
+       "  y = 0 : [0:1]\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  write: x := 1;\n"
+       "  fence;\n"
+       "  read: y = 0;\n"
+       "CS:\n"
+       "  write: x := 0;\n"
+       "  goto L1\n"
+       "process\n"
+       "text\n"
+       "L1:\n"
+       "  write: y := 1;\n"
+       "  fence;\n"
+       "  read: x = 0;\n"
+       "CS:\n"
+       "  write: y := 0;\n"
+       "  goto L1\n"
+       );
+
+    VipsBitReachability reach;
+
+    Arg arg(*m);
+    Result *res = reach.reachability(&arg);
+    Test::inner_test("#7 Simple Dekker",res->result == UNREACHABLE);
+
+    delete res;
+    delete m;
+  }
+  
 };
--- src/vips_bit_reachability.h
+++ src/vips_bit_reachability.h
@@ -22,6 +22,7 @@
 #define __VIPS_BIT_REACHABILITY__
 
 #include "reachability.h"
+#include "vips_bit_constraint.h"
 
 /* VipsBitReachability implements a reachability analysis specifically
  * for VipsBitConstraint. The analysis is forward and explicit state.
@@ -33,6 +34,28 @@ public:
   virtual Result *reachability(Arg *arg) const;
 
   static void test();
+private:
+  /* Each constraint will use a parent_t to keep track of its parent
+   * and the transition moving from the parent to itself.
+   *
+   * Both trans and parent are null if the constraint is an initial
+   * constraint.
+   */
+  struct parent_t{
+    parent_t() : trans(0), parent(0) {};
+    parent_t(const Machine::PTransition *t, const VipsBitConstraint *p)
+      : trans(t), parent(p) {};
+    const Machine::PTransition *trans;
+    const VipsBitConstraint *parent;
+  };
+
+  /* get_comparator(common) returns a function f that uses
+   * common.compare to compare VipsBitConstraint pointers.
+   *
+   * f(a,b) returns true iff common.compare(*a,*b) < 0.
+   */
+  typedef std::function<bool(const VipsBitConstraint*,const VipsBitConstraint*)> vbcmp_t;
+  static vbcmp_t get_comparator(const VipsBitConstraint::Common&);
 };
 
 #endif
